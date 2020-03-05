38c8de4c4dfd6c805d7f01bab08a6bbba6cd5cf6
Ronald Wabukenda <ronald.wabukenda@salesloft.com>
2020-03-05 16:45:06 -0500

    added frequency checker and run rubocop

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -1,10 +1,32 @@
+# frozen_string_literal: true
+
 class PeopleController < ApplicationController
-  before_action :use_my_key
   def index
+    get_data
+    render json: { request: JSON.parse(@response.body) }
+  end
 
-    # response = HTTParty.get('https://jsonplaceholder.typicode.com/posts')
-    response = HTTParty.get("#{SALESLOFT_URL}")
+  def frequency_checker
+    get_data
 
-    render json: {message: JSON.parse(response.body)}
+    all_data = @response['data']
+    all_emails_string = all_data.map { |person| person['email_address'] }.join
+
+    split_characters = all_emails_string.split('')
+    final_frequency = split_characters.group_by { |v| v }.map { |key, value| [value.length, key] }.to_h
+
+    render json: { message: final_frequency }
+    end
+
+  def get_data
+    @response = HTTParty.get(url, headers: { 'Authorization' => "Bearer #{api_key}" })
+  end
+
+  def api_key
+    api_key = ENV.fetch('SALESLOFT_API_KEY')
+  end
+
+  def url
+    url = ENV.fetch('SALESLOFT_URL')
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources  :people
+  resources :people
+
+  get '/frequency_checker', to: 'people#frequency_checker'
+  # '
 
   root 'people#home'
 end
-
